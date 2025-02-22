6013f1a930022fe8e4ae7188c6eefef53a507931
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2025-02-22 21:19:03 +0200

    update

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-# import os
-
-# class Config:
-#     SECRET_KEY = os.urandom(24)  # Change this to a secure key
-#     AWS_API_URL = "https://your-api-endpoint.amazonaws.com"
-
 import os
 from dotenv import load_dotenv
 from requests_aws4auth import AWS4Auth
