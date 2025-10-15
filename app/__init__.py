f119d18ac509ba1b10b1649c6a3ea3d57a1bf976
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2025-10-15 08:16:32 +0000

    update settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,3 @@
-# from flask import Flask, session
-# from config import Config
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object("config.Config")
-    
-#     from .routes.wallet import wallet_bp
-#     from .routes.transactions import transactions_bp
-#     from .routes.users import users_bp
-#     from .routes.auth import auth_bp
-    
-#     app.register_blueprint(wallet_bp)
-#     app.register_blueprint(transactions_bp)
-#     app.register_blueprint(users_bp)
-#     app.register_blueprint(auth_bp)
-    
-#     app.secret_key = Config.SECRET_KEY
-    
-#     return app
-
 
 from flask import Flask
 import os
@@ -29,7 +8,6 @@ from app.routes.crypto import crypto_bp
 from app.routes.stock import stock_bp
 from app.routes.settings import settings_bp
 from app.routes.auth import auth_bp
-# from app.routes.layout import layout_bp
 
 FLASK_SECRET_KEY = os.urandom(24)
 
@@ -44,5 +22,4 @@ def create_app():
     app.register_blueprint(stock_bp)
     app.register_blueprint(settings_bp)
     app.register_blueprint(auth_bp)
-    # app.register_blueprint(layout_bp)
     return app
--- app/routes/settings.py
+++ app/routes/settings.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 settings_bp = Blueprint('settings', __name__)
 
-@settings_bp.route('/setting', methods=['GET'])
+@settings_bp.route('/settings', methods=['GET'])
 def settings_page():
     user = session.get('user')
     if user:
@@ -17,6 +17,7 @@ def settings_page():
         try:
             response = requests.get(f"{API_URL}/settings", params={"userId": userId}, auth=aws_auth)
             settings = response.json().get("settings", []) if response.status_code == 200 else []
+            print(settings)
         except Exception as e:
             print(f"Error fetching settings: {e}")
             settings = []
@@ -26,40 +27,20 @@ def settings_page():
     else:
         return render_template("home.html")
 
-@settings_bp.route('/setting', methods=['POST'])
-def create_settings():
-    setting_id = str(uuid.uuid4())
-    user = session.get('user')
-    user_id = user.get('username')
-    data = {
-        "settingId": setting_id,
-        "userId": user_id,
-        "currency": request.form["currency"],
-        "theme": request.form["theme"]
-    }
-
-    print(data)
-    try:
-        response = requests.post(f"{API_URL}/setting", json=data, auth=aws_auth)
-        print(f"✅ [DEBUG] Create Response: {response.status_code}, JSON: {response.json()}")
-
-        return redirect(url_for("settings.settings_page"))
-    except Exception as e:
-        print(f"❌ [ERROR] Failed to create settings: {str(e)}")
-        return jsonify({"error": "Internal Server Error"}), 500
 
 @settings_bp.route('/updateSettings', methods=['POST'])
 def update_settings():
+    user = session.get('user')
+    user_id = user.get('username')
     data = {
-        "settingId": request.form["settingId"],
-        "userId": request.form["userId"],
+        "userId": user_id,
         "currency": request.form["currency"],
         "theme": request.form["theme"]
     }
     print(f"🔄 [DEBUG] Updating settings: {data}")
     
     try:
-        response = requests.patch(f"{API_URL}/setting", json=data, auth=aws_auth)
+        response = requests.patch(f"{API_URL}/settings", json=data, auth=aws_auth)
         print(f"✅ [DEBUG] Update Response: {response.status_code}, JSON: {response.json()}")
 
         return redirect(url_for("settings.settings_page"))
@@ -67,21 +48,5 @@ def update_settings():
         print(f"❌ [ERROR] Failed to update settings: {str(e)}")
         return jsonify({"error": "Internal Server Error"}), 500
 
-# @settings_bp.route('/deletesetting/<setting_id>/<user_id>', methods=['POST'])
-# def delete_setting(setting_id, user_id):
-#     """Delete a setting."""
-#     data = {
-#         "settingId": setting_id,
-#         "userId": user_id
-#     }
-#     print(f"🗑️ [DEBUG] Deleting setting: {data}")
 
-#     try:
-#         response = requests.delete(f"{API_URL}/setting", json=data, auth=aws_auth)
-#         print(f"✅ [DEBUG] Delete Response: {response.status_code}, JSON: {response.json()}")
-
-#         return redirect(url_for("setting.setting_page"))
-#     except Exception as e:
-#         print(f"❌ [ERROR] Failed to delete setting: {str(e)}")
-#         return jsonify({"error": "Internal Server Error"}), 500
     
