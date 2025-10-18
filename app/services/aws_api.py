99ae3e82c2b8e5e0dcf67ef15283414576026395
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2025-10-18 04:51:17 +0000

    cleanup

--- app/services/aws_api.py
+++
@@ -1,7 +0,0 @@
-# import requests
-# from config import Config
-
-# def call_lambda(endpoint, payload={}):
-#     url = f"{Config.AWS_API_URL}/{endpoint}"
-#     response = requests.post(url, json=payload)
-#     return response.json()
